b67b309c565a2a0f7ff536e676d98ae4aa0563e1
nguythehuy <43278120+nguythehuy@users.noreply.github.com>
2018-09-20 10:45:39 +0700

    Update Student.java

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -1,38 +1,62 @@
 package week1;
 
 public class Student {
-
-    // TODO: khai báo các thuộc tính cho Student
-
-    // TODO: khai báo các phương thức getter, setter cho Student
-
-    /**
-     * Constructor 1
-     */
-    Student() {
-        // TODO:
-    }
-
-    /**
-     * Constructor 2
-     * @param n
-     * @param sid
-     * @param em
-     */
-    Student(String n, String sid, String em) {
-        // TODO:
-    }
-
-    /**
-     * Constructor 3
-     * @param s
-     */
-    Student(Student s) {
-        // TODO:
-    }
-
-    String getInfo() {
-        // TODO:
-        return null; // xóa dòng này sau khi cài đặt
-    }
-}
+	private String name="Student";
+	private String id="000";
+	private String group="INT22041";
+	private String email="uet@vnu.edu.vn";
+	public String getName() {
+		return this.name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getId() {
+		return this.id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getGroup() {
+		return this.group;
+	}
+	public void setGroup(String group) {
+		this.group = group;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public void getInfo() {
+		System.out.println("Student INFO :");
+		System.out.println("- Name\t\t: "+this.name);
+		System.out.println("- Id\t\t: "+this.id);
+		System.out.println("- Group \t: "+this.group);
+		System.out.println("- Email \t: "+this.email);
+	}
+	public Student() {
+	}
+	public Student() {
+		this.name = n;
+		this.id = id;
+		this.email= em;
+	}
+	public Student(String n,String id,String group,String em) {
+		this.name =n;
+		this.id=id;
+		this.group=group;
+		this.email=em;
+	}
+	public Student(Student s) {
+		this.name = s.name;
+		this.id = s.id;
+		this.group = s.group;
+		this.email = s.email;
+	}
+} 
