1e43f207e743d20b084c27d1fd6e3750f8656f17
nguythehuy <43278120+nguythehuy@users.noreply.github.com>
2018-09-20 10:45:09 +0700

    Update StudentManagement.java

--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -1,23 +1,26 @@
 package week1;
 
 public class StudentManagement {
+    // cau 6
+    public static void main() {
+        Student Student1=new Student();
+        Student1.setName("Huy");
+        Student1.setId("17020799");
+        Student1.setGroup("INT22041");
+        Student1.setEmail("nguythehuy@gmail.com");
+        // in ra ten sv:
+        System.out.println(Student1.getName());
+        // in ra thong tin sv:
+        Student1.getInfo();
+        }
+    // cau 8
+    public static void main1() {
+		    Student Student2 = new Student();
+		    Student2.getInfo();
+		    Student Student3 = new Student("Huy","170207699","nguythehuy@gmail.com");
+		    Student3.getInfo();
+		    Student s = new Student(Student2);
+		    s.getInfo();	
+            }
+    
 
-    // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
-
-    public boolean sameGroup(Student s1, Student s2) {
-        // TODO:
-        return false; // xóa dòng này sau khi cài đặt
-    }
-
-    void studentsByGroup() {
-        // TODO:
-    }
-
-    void removeStudent(String id) {
-        // TODO:
-    }
-
-    public static void main(String[] args) {
-        // TODO:
-    }
-}
