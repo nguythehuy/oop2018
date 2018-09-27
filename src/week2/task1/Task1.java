df8f0fec3c2d3fe06bff1c59fdfaf62473c6d61f
nguythehuy <43278120+nguythehuy@users.noreply.github.com>
2018-09-27 10:36:23 +0700

    Update Task1.java

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -1,32 +1,24 @@
 package week2.task1;
 
 public class Task1 {
-
     public static int gcd(int a, int b) {
+	a=Math.abs(a);
+	b=Math.abs(b);
         // TODO: Tính ước chung lớn nhất của 2 số a, b
-        if(a==b){
-            return a;
-            }
-        else if(a>b){
-            return gcd(a-b,b);
-        }
-        else{ 
-            retun gcd(a;b-a);
-            }
-        return 0;
+	while (a!=b){
+		if(a>b) a=a-b;
+		else if(a<b) b=b-a;
+	}
+
+	return a;
+
     }
 
     public static int fibonacci(int n) {
         // TODO: Tìm số fibonacci ở vị trí n
-        if(n==0){
-            return 0;
-            }
-        else if (n==1){
-            return 1;
-            }
-        else{
-            return fibonacci(n-1)+fibonacci(n-2);
-            }
-        return 0;
+	
+        if(n==0) return 0;
+	else if(n==1) return 1;
+        else return fibonacci(n-1)+fibonacci(n-2);
     }
 }
