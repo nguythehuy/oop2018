30f1a4265071155c9731168315a7381679d7b9ee
nguythehuy <43278120+nguythehuy@users.noreply.github.com>
2018-10-11 09:45:47 +0700

    Create Tao.java

---
+++ src/week4/task1/Tao.java
@@ -0,0 +1,24 @@
+package week4.task1;
+
+public class Tao extends HoaQua {
+    public String thiTruong;
+    public int canNang;
+    /**
+    *
+    * Setter/getter về thị trường,cân nặng cho táo
+    *
+     */
+    public void setThiTruong(String thiTruong){
+        this.thiTruong=thiTruong;
+    }
+    public String getThiTruong(){
+        return thiTruong;
+    }
+    public void setCanNang(){
+        this.canNang=canNang;
+    }
+    public int getCanNang(){
+        return canNang;
+    }
+
+}
